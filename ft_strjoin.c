d39924d9a0dce34f56da9cd89e8b40a3aa71a21b
Romain Ballage <ballage.romain@gmail.com>
2020-01-07 14:07:42 +0100

    re

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: rballage <rballage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/12 19:32:04 by rballage          #+#    #+#             */
-/*   Updated: 2020/01/06 16:48:48 by rballage         ###   ########.fr       */
+/*   Updated: 2020/01/07 14:06:11 by rballage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,29 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		*str++ = *s1;
 
 	return (ft_strcat(ft_strcpy(str, s1), s2));
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	a;
+	size_t	b;
+	size_t	c;
+
+	c = ft_strlen(s2);
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	a = ft_strlen(s1);
+	if (!(str = malloc(sizeof(char) * (a + ft_strlen(s2) + 1))))
+		return (NULL);
+	i = -1;
+	b = 0;
+	while (++i < a)
+		str[i] = s1[i];
+	i--;
+	while (++i < a + c)
+		str[i] = s2[b++];
+	str[i] = '\0';
+	return (str);
 }
